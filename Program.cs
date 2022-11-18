e11c3ba9cb42a13bdf9022069054e5bd3046c4d7
Калкей Дмитрий <dumi55@mail.ru>
2022-11-18 20:06:06 +0300

    написал функцию возвращяющею доступность хоста

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +9,29 @@ namespace PingServer
 {
     internal class Program
     {
+
+        static string hostName; //ip adres or dns name host
+        static bool pingStatus = false; //статус ринга (есле ответ от хоста получен то true) 
+
+        static void Getping ()
+        {
+            pingStatus = false;
+            try
+            {
+                Ping ping = new Ping();
+                PingReply status = ping.Send(hostName, 5000);   // отправка запроса на доступность
+                pingStatus =( status.Status == IPStatus.Success) ;
+
+            }
+            catch
+            {
+                Console.WriteLine("проблеммы с сетью!!!");
+            }
+
+        }
+
+
+
         static void Main(string[] args)
         {
 
